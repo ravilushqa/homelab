bf6fc870226177c618bfd3c91b08e9cd1fd5a5b6
ravilushqa <galaktionov.r@gmail.com>
2025-01-01 11:40:51 +0100

    Update IP addresses across Kubernetes and Terraform configs to 192.168.1.x

--- terraform/modules/traefik/main.tf
+++ terraform/modules/traefik/main.tf
@@ -16,8 +16,8 @@ resource "proxmox_virtual_environment_container" "traefik" {
     hostname = "traefik"
     ip_config {
       ipv4 {
-        address = "192.168.2.3/24"
-        gateway = "192.168.2.1"
+        address = "192.168.1.4/24"
+        gateway = "192.168.1.1"
       }
     }
     user_account {
@@ -46,7 +46,7 @@ resource "proxmox_virtual_environment_container" "traefik" {
 
   connection {
     type = "ssh"
-    host = "192.168.2.3" # Container's IP
+    host = "192.168.1.4" # Container's IP
     user = "root"
     private_key = trimspace(tls_private_key.ubuntu_container_key.private_key_openssh)
   }
